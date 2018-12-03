e238293785d307588b433687070e08e1375c007b
王妍军 <wangyanjunping@live.com>
2018-12-03 22:41:20 +0800

    add more infras.

---
+++ src/core/inc.core/inc.core/basis/OPResult_T.cs
@@ -0,0 +1,34 @@
+﻿namespace inc.core
+{
+    /// <summary>
+    /// 操作结果
+    /// </summary>
+    /// <typeparam name="T">数据类型</typeparam>
+    public class OpResult<T> : OpResult
+    {
+        /// <summary>
+        /// 获取或设置内容
+        /// </summary>
+        public T Content { get; set; }
+
+        /// <summary>
+        /// 构造OpResult
+        /// </summary>
+        public OpResult() { }
+
+        /// <summary>
+        /// 构造OpResult
+        /// </summary>
+        /// <param name="content">数据</param>
+        public OpResult(T content)
+        {
+            Content = content;
+        }
+
+        /// <summary>
+        /// 获取内容
+        /// </summary>
+        /// <returns>附加的数据内容</returns>
+        public override object GetContent() => IsSuccess ? Content : default(object);
+    }
+}
