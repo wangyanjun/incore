79c21c9cdafd71c1a37d5f5d36e59081eca6550b
王妍军 <wangyanjunping@live.com>
2018-12-06 20:39:11 +0800

    add more builds.

--- src/core/inc.core/pack.cmd
+++ src/core/inc.core/pack.cmd
@@ -1,2 +1,5 @@
 dotnet pack ./inc.core/inc.core.csproj --include-symbols -c release --output ../build/inc.core
-dotnet pack ./protocols/inc.protocols.finsnet/inc.protocols.finsnet.csproj --include-symbols -c release --output ../../build/inc.protocols.finsnet
+dotnet pack ./inc.protocols.finsnet/inc.protocols.finsnet.csproj --include-symbols -c release --output ../build/inc.protocols.finsnet
+dotnet pack ./inc.protocols.mc/inc.protocols.mc.csproj --include-symbols -c release --output ../build/inc.protocols.mc
+dotnet pack ./inc.protocols.modbus/inc.protocols.modbus.csproj --include-symbols -c release --output ../build/inc.protocols.modbus
+dotnet pack ./inc.plcs.omron/inc.plcs.omron.csproj --include-symbols -c release --output ../build/inc.plcs.omron
