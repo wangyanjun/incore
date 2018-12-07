05c4fbebad019aebaf9d6490016f239429f413a6
王妍军 <wangyanjunping@live.com>
2018-12-07 20:10:42 +0800

    add more unit test about modbus.

---
+++ src/tests/inc.tests/inc.protocols.modbus.tests/ReadCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace inc.protocols.modbus.tests
+{
+    public class ReadCommandTests
+    {
+        [Fact]
+        public void Encode()
+        {
+            var command = new ReadCommand()
+            {
+                Address = 0x12,
+                Code = ModbusFunctionCode.ReadCoils,
+                Count = 2
+            };
+
+            var content = command.Encode();
+            Assert.Equal(5, content.Length);
+        }
+    }
+}
